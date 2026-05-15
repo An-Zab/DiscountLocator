dbf94366e612ab6e5ebe0b802dd93f31c5352372
An-Zab <96343812+An-Zab@users.noreply.github.com>
2026-05-15 22:50:44 +0300

    Парсинг ссылок на офферы итерация 1

--- parser.py
+++ parser.py
@@ -11,3 +11,16 @@ def get_page_max_num(htmlpage):
     else:
         return 1
 
+
+# with open('results/1k.by_1_response.html', 'r', encoding='UTF-8') as file:
+#     html_page_content = file.read()	
+
+def receive_offer_place(htmlpage):
+    soup = BeautifulSoup(htmlpage, 'lxml')
+    links = []
+    for link in soup.find_all(class_="prod__ext"):
+        links.append(link.get('href'))
+    return links
+
+# print(receive_offer_place(html_page_content))
+
